f73d6d27857682533484878d7d5a322f62baf64d
Victhor Gabriel <victhorgabriel00@live.com>
2023-11-18 13:52:10 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form-telefone')
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-lista');
+    const inputNumeroContato = document.getElementById('número-lista');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += `</td>`
+
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
